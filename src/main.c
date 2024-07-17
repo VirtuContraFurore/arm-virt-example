d85afbf4624292a38194d801d635a5133874f1bf
Ceragioli Luca° <ceragioli_luca@libero.it>
2024-07-18 01:27:34 +0200

    fixed pc incr

--- src/main.c
+++ src/main.c
@@ -18,11 +18,14 @@ void install_el2_vbar();
 void start_guest_VM();
 
 void guest_main(){
-    puts("Guest entered");
-    while(1){
+    puts("GUEST: hello main!");
+    for(int i=0; i<5;i++){
         asm volatile("wfi");
-        puts("guest alive");
+        puts("GUEST: alive after WFI");
+        asm volatile("wfe");
+        puts("GUEST: alive after WFE");
     }
+    puts("GUEST: VM done...");
 }
 
 void main() {
@@ -38,18 +41,18 @@ int check_execution_level(){
     unsigned long el;
     asm volatile("mrs %0, CurrentEL": "=r" (el));
     if( (el >> 2) == 2) {
-        puts("Hypervisor BOOTED in EL2");
+        puts("HYPERV: Hypervisor BOOTED in EL2");
         return EL2;
     } else {
-        puts("Hypervisor is not in EL2, is virtualization=on ?");
-        puts("Exiting...");
+        puts("HYPERV: Hypervisor is not in EL2, is virtualization=on ?");
+        puts("HYPERV: Exiting...");
         return EL1;
     }
 }
 
 void install_el2_vbar(){
     asm volatile("msr vbar_el2, %0": : "r" (el2_vec_table));
-    puts("VBAR_EL2 installed.");
+    puts("HYPERV: VBAR_EL2 installed.");
 }
 
 void start_guest_VM(){
@@ -60,8 +63,9 @@ void start_guest_VM(){
 
     asm volatile("msr elr_el2, %0" : : "r" (start_guest)); // set guest entry point
     asm volatile("msr spsr_el2, %0" : : "r" (0)); // clear guest saved program state register
+    //asm volatile("msr sctlr_el1, %0" : : "r" (0)); // clear
     asm volatile("msr sp_el1, %0" : : "r" (_guest_stack_start)); // setup guest stack
-    puts("Startin VM...");
+    puts("HYPERV: Starting VM...");
     asm volatile("eret"); // start guest VM
 }
 
@@ -70,16 +74,20 @@ void dummy_vector(){
 }
 
 void hypervisor_exception_vector(){
-    puts("Exception caught from ELx, x < 2");
+    puts("HYPERV: Exception caught from EL1/0");
     /* Now read excpetion syndrome */
-    unsigned long esr_el2;
+    unsigned long esr_el2, elr_el2;
     asm volatile("mrs %0, esr_el2" : "=r" (esr_el2));
+    asm volatile("mrs %0, elr_el2" : "=r" (elr_el2));
     unsigned long ec = esr_el2 >> 26;
     if(ec == 0x01) {
         if(esr_el2 & 1)
-            puts("trapped WFE");
+            puts("HYPERV: trapped WFE");
         else
-            puts("trapped WFI");
-    }
+            puts("HYPERV: trapped WFI");
+    } else
+        puts("HYPERV: unrecognized exception syndrome");
+    elr_el2 += 4; // move to next instr
+    asm volatile("msr elr_el2, %0" : : "r" (elr_el2)); // updated PC
     asm volatile("eret"); // return from exception
 }
